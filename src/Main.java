39eee79b4d848f32a9c7ea30441e66d285815ca6
romanmihailow <romanmihailow@email.com>
2023-01-09 16:41:11 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        int money = 3000; // потраченная сумма
+        int oneMileage = 20; // 20 потраченных рублей равны 1 миле
+        int sumMileage = money / oneMileage ;
+
+
+
+        System.out.print("За купленный билет начислено миль:" + sumMileage );
+
+
+    }
+
+
+
+
+
+}
